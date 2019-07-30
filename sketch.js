b5f6d745c9eb5034a0eca8553b2c987c5af71f0a
WrrngnCode <ferenc.bujdoso@outlook.com>
2019-07-30 19:56:01 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+/// <reference path="./node_modules/@types/p5/global.d.ts" />
+let canv;
+let container;
+let lblInfo;
+
+function setup() {
+
+    canv = createCanvas(300, 300);
+    container = document.getElementById('sketch-div');
+    container.appendChild(canv.elt);
+    lblInfo = document.getElementById("lblInfo");
+
+}
+
+
+
+function draw(){
+
+
+
+
+
+
+}
